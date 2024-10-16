147cf951433a6e5daa8103db662383d5ea9c7a66
Alex Kempton <alex@funwithtriangles.net>
2024-10-16 19:09:43 +0200

    path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+
 export default async function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPassthroughCopy("./src/style.css");
 
   return {
