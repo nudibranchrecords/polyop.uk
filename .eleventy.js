382d27a48dc88371b1b37e5d935ca096fcd782f4
Alex Kempton <alex@funwithtriangles.net>
2025-02-07 15:42:03 +0100

    nav glitch effect

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addWatchTarget("./src/js/");
   eleventyConfig.addPassthroughCopy("./src/img");
 
   return {
---
+++ src/js/glitchnav.js
@@ -0,0 +1,31 @@
+const navItems = document.querySelectorAll(".glitch-nav-item");
+const icons = window.allSymbols;
+const GLITCH_INTERVAL = 120;
+
+let isGlitching = false;
+let glitchIcon = null;
+let iconIndex = 0;
+
+const glitch = () => {
+  if (isGlitching) {
+    glitchIcon.textContent = icons[iconIndex];
+    iconIndex = (iconIndex + 1) % icons.length;
+  }
+};
+
+setInterval(glitch, GLITCH_INTERVAL);
+
+navItems.forEach((item) => {
+  const icon = item.querySelector("span");
+  icon.dataset.originalSymbol = icon.textContent;
+  item.addEventListener("mouseover", () => {
+    glitchIcon = icon;
+    isGlitching = true;
+  });
+  item.addEventListener("mouseout", () => {
+    isGlitching = false;
+    requestAnimationFrame(() => {
+      icon.textContent = icon.dataset.originalSymbol;
+    });
+  });
+});
